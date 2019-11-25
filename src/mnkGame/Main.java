c9470eb7a6d5b11fb69a03526193c9396ae3ba12
Artem Labazov <123321artyom@gmail.com>
2019-11-25 15:13:29 +0300

    Tournament support

--- src/mnkGame/Main.java
+++ src/mnkGame/Main.java
@@ -5,12 +5,44 @@ package mnkGame;
  */
 public class Main {
     public static void main(String[] args) {
-        final Game game = new Game(false, new HumanPlayer(), new HumanPlayer());
-        int result;
-        do {
-            System.out.println("\n\n\n");
-            result = game.play(new MnkBoard(8, 5, 4));
-            System.out.println("Game result: " + result);
-        } while (result != 0);
+        final int c = 3;
+        final int[][] score = new int[c][c];
+        final Game game = new Game(false, new RandomPlayer(), new RandomPlayer());
+
+        for (int i = 0; i < c; i++) {
+            for (int j = i + 1; j < c; j++) {
+                System.out.println("\n\n");
+                int result = game.play(new MnkBoard(8, 5, 4));
+                System.out.println("Game result: " + result);
+
+                switch (result) {
+                    case 0:
+                        score[i][j] = 1;
+                        score[j][i] = 1;
+                        break;
+                    case 1:
+                        score[i][j] = 3;
+                        break;
+                    case 2:
+                        score[j][i] = 3;
+                        break;
+                }
+            }
+        }
+
+        StringBuilder resultTable = new StringBuilder(" ");
+        for (int i = 0; i < c; i++) {
+            resultTable.append(" ");
+            resultTable.append(i + 1);
+        }
+        for (int i = 0; i < c; i++) {
+            resultTable.append("\n");
+            resultTable.append(i + 1);
+            for (int j = 0; j < c; j++) {
+                resultTable.append(" ");
+                resultTable.append(score[i][j]);
+            }
+        }
+        System.out.println(resultTable.toString());
     }
 }
